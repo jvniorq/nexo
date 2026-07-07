d0b66d7ea19da654ed13ee84ccce10c2f8fde63b
juniorxo <wilsongrana2@gmail.com>
2026-07-07 18:49:32 -0500

    Modularize domain models and theme
    Removed unused classes and NexoTheme implementation from main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'core/nexo_theme.dart';
+import 'domain/models.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const NexoApp());
-}
-
-enum NexoSpace { personal, household }
-
-class TaskItem {
-  TaskItem(this.title, this.space, {this.completed = false});
-
-  final String title;
-  final NexoSpace space;
-  bool completed;
-}
-
-class ShoppingEntry {
-  ShoppingEntry(this.name, this.quantity, {this.completed = false});
-
-  final String name;
-  final String quantity;
-  bool completed;
-}
-
-class NoteEntry {
-  NoteEntry(this.title, this.content, this.space);
-
-  final String title;
-  final String content;
-  final NexoSpace space;
 }
 
 class NexoApp extends StatefulWidget {
@@ -52,73 +29,6 @@ class _NexoAppState extends State<NexoApp> {
       home: NexoShell(
         themeMode: _themeMode,
         onThemeChanged: (mode) => setState(() => _themeMode = mode),
-      ),
-    );
-  }
-}
-
-abstract final class NexoTheme {
-  static ThemeData get light => _build(
-        Brightness.light,
-        const Color(0xFFF7F7F4),
-        Colors.white,
-        const Color(0xFF526F5B),
-      );
-
-  static ThemeData get dark => _build(
-        Brightness.dark,
-        const Color(0xFF111310),
-        const Color(0xFF1B1E1A),
-        const Color(0xFF8CB398),
-      );
-
-  static ThemeData _build(
-    Brightness brightness,
-    Color background,
-    Color surface,
-    Color primary,
-  ) {
-    final scheme = ColorScheme.fromSeed(
-      seedColor: primary,
-      brightness: brightness,
-      surface: surface,
-    );
-    return ThemeData(
-      useMaterial3: true,
-      brightness: brightness,
-      colorScheme: scheme,
-      scaffoldBackgroundColor: background,
-      cardTheme: CardThemeData(
-        color: surface,
-        elevation: 0,
-        margin: EdgeInsets.zero,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-      ),
-      navigationBarTheme: NavigationBarThemeData(
-        backgroundColor: surface,
-        elevation: 0,
-        indicatorColor: scheme.primaryContainer,
-      ),
-      navigationRailTheme: NavigationRailThemeData(
-        backgroundColor: surface,
-        indicatorColor: scheme.primaryContainer,
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: scheme.primary,
-        foregroundColor: scheme.onPrimary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18),
-        ),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: surface,
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16),
-          borderSide: BorderSide.none,
-        ),
       ),
     );
   }
